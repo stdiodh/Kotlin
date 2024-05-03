50354f70cb09e823ce165f446fb797c89083c3d3
stdiodh <playlistdh@gmail.com>
2024-05-03 19:59:31 +0900

    numberVar

--- src/numberVar.kt
+++ src/numberVar.kt
@@ -1,6 +1,13 @@
+import kotlin.reflect.typeOf
+
 fun main (){
     var number1 : Int = 20
     var number2 : Double = 2.0
     var number3 : Long = 100
 
+    if (number1 is Int) {
+        println("number1 is Int")
+    } else {
+        println("number2 is not Int")
+    }
 }
