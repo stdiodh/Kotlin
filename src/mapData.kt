8543cf59117733e244139a3c68e5129d9560a637
stdiodh <playlistdh@gmail.com>
2024-05-03 19:41:58 +0900

    mapData

--- src/mapData.kt
+++ src/mapData.kt
@@ -1,4 +1,7 @@
 fun main() {
+//    mapData는 변수를 리스트화한 것
+//    mapOf는 리스트의 변수가 값이 변경이 안됨.
+
 //    var myMap = mapOf(
 //        "name" to "빵빵이",
 //        "age" to 24,
@@ -8,6 +11,7 @@ fun main() {
 //    println(myMap)
 //    println(myMap["name"])
 
+//  mutableMapOf는 리스트안 변수를 변경가능
     var myMap = mutableMapOf(
         "name" to "빵빵이",
         "age" to 24,
