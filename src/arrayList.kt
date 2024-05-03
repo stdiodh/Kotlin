a7c8aeea0baf26005112d86b61c0e7f6741cf6b5
stdiodh <playlistdh@gmail.com>
2024-05-03 20:07:58 +0900

    arrayList

--- src/arrayList.kt
+++ src/arrayList.kt
@@ -1,10 +1,13 @@
 fun main(){
+//    코드 유지보수 상으로 배열 뒤 ,를 사용하는 것이 좋음
 //    var arr = listOf(1, 2, 3, 4, 5, )
 //    println(arr)
 //    println(arr[0])
 //    arr[2] = 3 이 불가능
 
-    var arr = mutableListOf(1, 2, 3, 4, 5,)
+
+//  mutable의 차이는 나중에 배열의 원소들을 바꿀 수 있는지의 차이
+    var arr = mutableListOf(1, 2, 3, 4, 5, )
     println(arr)
     println(arr[2])
 
