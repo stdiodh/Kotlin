d07e7f816c99f873769a91c501f6239aa281f3b5
stdiodh <playlistdh@gmail.com>
2024-05-03 19:46:06 +0900

    nullSafety

--- src/nullSafety.kt
+++ src/nullSafety.kt
@@ -1,6 +1,9 @@
 fun main(){
+//  swift의 옵셔널 변수와 비슷함
+//  값을 가질수도 안 가질수도 있는 타입
     var age : Int? = null
     var sum : Int = 0
+
 //    age = 20
 //    if (age != Null) {
 //        sum = age!! + 10
