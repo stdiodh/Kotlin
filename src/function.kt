45f5038f239ce3ca69e7b20f02db9dadeac75c97
stdiodh <playlistdh@gmail.com>
2024-05-03 19:55:50 +0900

    function

--- src/function.kt
+++ src/function.kt
@@ -8,9 +8,11 @@ fun sayHello() : Unit{
     println("Hello")
 }
 
+//fun 함수이름 (매개변수 이름1 : 타입1, 매개변수 이름2 : 타입2) : 리턴변수타입 {}
 fun add (a : Int, b : Int) : Int {
     return a + b
 }
 
 fun div (a : Int, b : Int) : Int = a-b
-//=으로 한줄로 축약가능
+
+//함수가 한줄일 때는 =으로 축약가능함
