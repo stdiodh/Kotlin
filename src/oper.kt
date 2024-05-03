42be02a854764f67f1f3d124cebe0e78b76c2a0e
stdiodh <playlistdh@gmail.com>
2024-05-03 19:33:19 +0900

    oper

--- src/oper.kt
+++ src/oper.kt
@@ -1,10 +1,10 @@
-fun main () {
-//    var a : Int = 3
-//    var b : Int = if(a == 5) 8 else 3
-//
-//    println(b)
+fun main(){
+    var a : Int = 3
+    var b : Int = if(a == 5) 8 else 3
+    //b는 엘비스 연산자로 a의 값에 따라 값이 결정됨.
+    println(b)
 
-    
-//    var nubmer : Int? = 10
-//    println(number ?: "null인 값이였음.")
+
+    var nubmer : Int? = 10
+    println(number ?: "null인 값이였음.")
 }
