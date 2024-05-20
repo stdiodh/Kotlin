32f4d03b8fce5c40a09fb01cde80c5e5eb9313e2
stdiodh <playlistdh@gmail.com>
2024-05-20 17:46:47 +0900

    상속, 오버라이드

---
+++ src/extands.kt
@@ -0,0 +1,26 @@
+fun main(){
+
+}
+
+//코틀린은 기본적으로 닫혀있음
+//상속을 받으려면 open을 해야함
+
+abstract class Animal {
+    //또한 오버라이드를 위해 open이라는 키워드가 필요
+    open fun move(){
+        println("이동")
+    }
+}
+
+
+class Dog : Animal(){
+    override fun move(){
+        println("껑충")
+    }
+}
+
+class Cat : Animal(){
+    override fun move(){
+        println("살금")
+    }
+}
