3fcb3560f679f802ff049d1b3faf7d6845137bfd
stdiodh <playlistdh@gmail.com>
2024-05-20 17:32:18 +0900

    게터, 세터 추가

--- src/class.kt
+++ src/class.kt
@@ -3,11 +3,12 @@ fun main(){
     println(john.name)
     println(john.age)
 
-    //세터 : 필드의 값을 초기화하고 새 값을 넣음
+    //setter(세터로 필드의 값을 초기화하고 새 값을 넣음)
     john.age = 21
 }
 
 // {} brace 에 사용하는 것이 아닌 () parentheses 에 사용
+//()안의 값은 getter(게터로 필드 안의 값을 제공함)
 class Person(val name : String, var age : Int,) {
 
 }
