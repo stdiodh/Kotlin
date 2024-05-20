d2393c9d93555ad09e872fc3f828fa6db4749325
stdiodh <playlistdh@gmail.com>
2024-05-20 17:21:48 +0900

    함수 오버로드 추가

--- src/function.kt
+++ src/function.kt
@@ -2,8 +2,11 @@ fun main() {
     sayHello()
     println(add(3, 5))
     println(div(3, 5))
+    println(add(3, 5, 1))
 }
 
+//main 함수 밖에서 선언된 함수는 toplevel함수로
+//어느 파일에서도 사용가능한 함수이다.
 fun sayHello() : Unit {
     println("Hello")
 }
@@ -13,6 +16,8 @@ fun add (a : Int, b : Int) : Int {
     return a + b
 }
 
+//함수가 한줄일 때는 =으로 축약가능함
 fun div (a : Int, b : Int) : Int = a-b
 
-//함수가 한줄일 때는 =으로 축약가능함
+//c가 없지만 오버로드를 통해 c를 디폴트로 만들어 사용할 수도 있음
+fun add (a: Int, b: Int, c : Int = 1) = a+b+c
--- src/helloWorld.kt
+++ src/helloWorld.kt
@@ -1,3 +1,5 @@
 fun main() {
     println("Hello World!")
+    //top레벨 함수
+    sayHello()
 }
