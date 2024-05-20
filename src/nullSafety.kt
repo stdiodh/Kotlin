9e28bfd0369d071a9bfe6a5fe2acd08b97e1a177
stdiodh <playlistdh@gmail.com>
2024-05-20 17:16:04 +0900

    nullSafety

--- src/nullSafety.kt
+++ src/nullSafety.kt
@@ -4,6 +4,21 @@ fun main(){
     var age : Int? = null
     var sum : Int = 0
 
+    var name: String? = null
+    name = "춘식"
+    var name2: String = ""
+
+    //?를 붙인 순간 타입을 달라짐 널 체크를 하고 넣음
+//    if (name != null) {
+//        name2 = name
+//    }
+
+    //위의 조건문과 동일하며 if문 대신 간단하게 사용가능
+    name?.let{
+        name2 = name
+    }
+    println(name2)
+
 //    age = 20
 //    if (age != Null) {
 //        sum = age!! + 10
