ddaa6052b5697344c7570f2665bdc9a859b3e1c6
stdiodh <playlistdh@gmail.com>
2024-05-20 18:15:58 +0900

    callBack

---
+++ src/callBack.kt
@@ -0,0 +1,14 @@
+fun main() {
+    //인자가 밖으로 빠질 수 있음
+    myFunc(10){
+        println("함수 호출")
+    }
+}
+
+//input도 없고 return도 없다.
+//앞에 suspend를 붙이면 정지 함수
+fun myFunc(a:Int, callBack: () -> Unit){
+    println("함수 시작")
+    callBack()
+    println("함수 끝")
+}
