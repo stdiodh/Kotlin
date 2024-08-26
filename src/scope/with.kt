832c85e77b2421d41c0120b709a6b89034e064ff
stdiodh <playlistdh@gmail.com>
2024-08-26 16:16:19 +0900

    scope

---
+++ src/scope/with.kt
@@ -0,0 +1,26 @@
+package scope
+
+class Book(var name : String, var price: Int){
+    fun discount() {
+        price -= 5000
+    }
+}
+
+fun main() {
+    val a = Book("Kotlin의 모험", 30000).apply {
+        name = "[세일중] $name"
+        discount()
+    }
+
+    println("상품명 : ${a.name}, 가격 : ${a.price}")
+
+//    val bookCost = a.run {
+//        price + 5000
+//    }
+//    println("원가는 $bookCost!")
+//
+    val bookCost = with(a) {
+        price + 5000
+    }
+    println("원가는 $bookCost!")
+}
