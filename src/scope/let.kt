f45f8cd49cfc91ced59f6383d29e5a985b6c2906
stdiodh <playlistdh@gmail.com>
2024-08-26 15:20:01 +0900

    let

--- src/scope/let.kt
+++ src/scope/let.kt
@@ -1,4 +1,23 @@
 package scope
 
-class let {
+fun main() {
+//    val numbers = mutableListOf("one", "two", "three", "four", "five")
+//    val resultList = numbers.map { it.length }.filter { it > 3 }
+//    println(resultList)
+//
+//    val numbers = mutableListOf("one", "two", "three", "four", "five")
+//    numbers.map { it.length }.filter { it > 3 }.let(::println)
+
+//    val str: String? = "Hello"
+//    val length = str?.let {
+//        println("let()에 의해 호출됨 $it")
+//        it.length
+//    }
+//
+    val numbers = listOf("one", "two", "three", "four")
+    val modifiedFirstItem = numbers.first().let { firstItem ->
+        println("The first item of the list is '$firstItem'")
+        if (firstItem.length >= 5) firstItem else "!" + firstItem + "!"
+    }.uppercase()
+    println("First item after modifications: '$modifiedFirstItem'")
 }
