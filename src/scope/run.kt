bc159894b545c5f07091339403d7dabad8c8211a
stdiodh <playlistdh@gmail.com>
2024-08-26 16:10:08 +0900

    kotlin

--- src/scope/run.kt
+++ src/scope/run.kt
@@ -1,4 +1,21 @@
 package scope
 
-class run {
-}
+//class Book(var name : String, var price: Int){
+//    fun discount() {
+//        price -= 5000
+//    }
+//}
+//
+//fun main() {
+//    val a = Book("Kotlin의 모험", 30000).apply{
+//        name = "[세일중] $name"
+//        discount()
+//    }
+//
+//    println("상품명 : ${a.name}, 가격 : ${a.price}")
+//
+//    val bookCost = a.run {
+//        price + 5000
+//    }
+//    println("원가는 $bookCost!")
+//}
