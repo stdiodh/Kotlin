00004c9b73a517a9bac15efb6b3ec87301179167
stdiodh <playlistdh@gmail.com>
2024-09-06 12:38:46 +0900

    highOrder

--- src/lambda/highOrder.kt
+++ src/lambda/highOrder.kt
@@ -1,8 +1,11 @@
 package lambda
 
 fun main() {
+    // 함수를 변수로 저장
     val apple = { fruit : String -> println("fruit : $fruit") }
-
     apple.invoke("apple")
 
+    // 함수를 파라미터로 전달
+    val bucket = listOf("apple", "banana", "strawberry", "watermelon").fold("tomato")
+    { acc: String, next: String -> "$acc, $next"}.also { println(it) }
 }
