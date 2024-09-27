37f960c47d0d2edf30216e30b2d861d572cd7193
stdiodh <playlistdh@gmail.com>
2024-09-27 19:07:02 +0900

    ArrayList

---
+++ src/CS/dataStructure/ArrayList.kt
@@ -0,0 +1,12 @@
+package CS.dataStructure
+
+fun main(){
+    val numArrayList = arrayListOf<Int>(1,2,3,4,5,6)
+    numArrayList.add(7)
+    numArrayList.add(8)
+    numArrayList.add(9)
+    numArrayList.add(10)
+
+    println(numArrayList)
+}
+
