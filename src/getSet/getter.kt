0a9e1ebb954cee88e8de908cc4c65f0c7e3bc0ea
stdiodh <playlistdh@gmail.com>
2025-01-10 17:03:26 +0900

    setter

--- src/getSet/getter.kt
+++ src/getSet/getter.kt
@@ -1,5 +1,6 @@
 package getSet
 
+/*
 private class Person {
     var name: String = "John" // 기본 값
         get() = field // Getter: 값을 반환함
@@ -9,3 +10,4 @@ fun main() {
     val person = Person()
     println("이름: ${person.name}")  // Getter 호출 (출력: 이름: John)
 }
+**/
---
+++ src/getSet/setter.kt
@@ -0,0 +1,14 @@
+package getSet
+
+private class Person {
+    var name: String = "John"
+        set(value) {  // Setter: 값을 수정함
+            field = value
+        }
+}
+
+fun main() {
+    val person = Person()
+    person.name = "Alice"  // Setter 호출 (값을 변경)
+    println("이름: ${person.name}")  // 변경된 값 출력
+}
