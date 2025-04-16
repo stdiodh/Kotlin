bd61ba2d3947c52978fbad39b1faee21a616432b
stdiodh <playlistdh@gmail.com>
2025-04-16 22:18:23 +0900

    abstract

--- src/Class/Animal.kt
+++ src/Class/Animal.kt
@@ -1,15 +1,29 @@
 package Class
 
-open class Animal {
-    open fun speak() = println("동물 소리")
+//open class Animal {
+//    open fun speak() = println("동물 소리")
+//}
+//
+//class Cat : Animal() {
+//    override fun speak() {
+//        println("야옹!")
+//    }
+//}
+//
+//fun main() {
+//    val cat = Cat().speak() //야옹!
+//}
+
+abstract class Animal {
+    abstract fun sound()
 }
 
 class Cat : Animal() {
-    override fun speak() {
+    override fun sound() {
         println("야옹!")
     }
 }
 
-fun main() {
-    val cat = Cat().speak() //야옹!
+fun main(){
+    val cat = Cat() // 야옹!
 }
