2d585d43846ce17ff43e35fc09e556e9edf842d6
stdiodh <playlistdh@gmail.com>
2025-04-16 21:19:28 +0900

    class

--- src/Class/class.kt
+++ src/Class/class.kt
@@ -2,10 +2,13 @@ package Class
 
 class User (val name : String, val age : Int){
     init {
+        if(name.length > 3){
+            throw IllegalArgumentException("이름은 3글자를 넘어갈 수 없습니다.")
+        }
         println("User가 생성되었습니다! 이름 : $name, 나이 : $age")
     }
 }
 
 fun main(){
-    val newUser = User("DH", 24) //User가 생성되었습니다! 이름 : DH, 나이 : 24
+    val newUser = User("HDH1234", 24) //Exception in thread "main" java.lang.IllegalArgumentException: 이름은 3글자를 넘어갈 수 없습니다.
 }
