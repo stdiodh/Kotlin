916db612cc93e72c61a7dd7a75a834c441b421a4
stdiodh <playlistdh@gmail.com>
2025-04-24 21:28:18 +0900

    Modifiers

--- src/Class/visibility Modifiers.kt
+++ src/Class/visibility Modifiers.kt
@@ -46,9 +46,48 @@ enum class Direction(val desc: String) {
     NORTH("북"), SOUTH("남"), EAST("동"), WEST("서")
 }
 
-annotation class MyAnnotation
+//annotation class MyAnnotation
+//
+//@MyAnnotation
+//fun main(){
+//
+//}
 
-@MyAnnotation
-fun main(){
+class UserInfo {
+    var name: String = ""
+        set(value) {
+            if (value == "김연아")
+                field = "${value}는 천재입니다"
+            else field=value
+        }
 
+    var address: String = ""
+        set(value) {
+            if (value == "한국") {
+                field = "대한민국"
+            } else field = value
+        }
+        get() {
+            // 반드시 return 문으로 작성해야 합니다.
+            return "${field}에 거주하고 있습니다"
+        }
+
+    constructor(name: String, address: String) {
+        this.name = name
+        this.address = address
+    }
+}
+
+fun main() {
+    val user1 = UserInfo("김연아", "한국")
+    println(user1.name)
+    //김연아는 천재입니다
+    println(user1.address)
+    //대한민국에 거주하고 있습니다
+
+    val user2 = UserInfo("홍길동", "서울")
+    println(user2.name)
+    //홍길동
+    println(user2.address)
+    //서울에 거주하고 있습니다
 }
