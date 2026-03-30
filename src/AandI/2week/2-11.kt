252e989d8ff109fcb899c1cda9e2b21a6c0e38e7
stdiodh <playlistdh@gmail.com>
2026-03-30 20:27:40 +0900

    3/30 practice 2-11

---
+++ src/AandI/2week/2-11.kt
@@ -0,0 +1,14 @@
+package AandI.`2week`
+
+//fun solution(fileName: String) : String {
+//    var answer = ""
+//    var splitExtension = fileName.split(".")
+//    val splitCnt = splitExtension.size - 1
+//    answer = if(splitCnt == 0) {
+//        "NONE"
+//    } else {
+//        splitExtension[splitCnt]
+//    }
+//
+//    return answer
+//}
